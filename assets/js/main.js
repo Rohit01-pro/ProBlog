29ba30e33f74ce2b8bbc2e6cc4a5de2386b3d45a
Rohit01-pro <rohit777kanojiya@gmail.com>
2021-05-28 19:22:23 +0530

    Git initialized

---
+++ assets/js/main.js
@@ -0,0 +1,103 @@
+const menuIcon = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon');
+const sideNav = document.querySelector('#side-nav');
+const overlay = document.querySelector('#overlay');
+
+
+menuIcon.addEventListener("click", ()=>{
+  sideNav.classList.add("slide");
+  overlay.classList.add("show");
+});
+
+closeIcon.addEventListener("click", ()=>{
+  sideNav.classList.remove("slide");
+  overlay.classList.remove("show");
+});
+
+overlay.addEventListener("click", ()=>{
+  sideNav.classList.remove("slide");
+  overlay.classList.remove("show");
+});
+
+
+// Search Box
+const searchIcon = document.querySelector(".search-icon");
+
+searchIcon.addEventListener("click",()=>{
+  window.location = "search.html";  
+});
+
+
+//  Scroll Events
+const navBar = document.querySelector("#navbar");
+const scrollToTopBtn = document.querySelector("#scrollToTopBtn");
+const rootElement = document.documentElement;
+var prevYpos = window.pageYOffset;
+
+window.onscroll = () => {
+  // Scroll to top button
+  if (rootElement.scrollTop > 400) {
+    scrollToTopBtn.style.right = "20px";
+  } else if (rootElement.scrollTop < 300) {
+    scrollToTopBtn.style.right = "-60px";
+  }
+
+  // Navbar slide
+  var currentYpos = window.pageYOffset;
+  if (prevYpos > currentYpos) {
+    navBar.style.top = "0";
+  } else {
+    navBar.style.top = "-100%";
+  }
+  prevYpos = currentYpos;
+  };
+
+
+// Scroll to Top
+scrollToTopBtn.addEventListener("click", ()=>{
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+
+
+
+
+
+
+// Banner Owl Carousel
+$(document).ready(function () {
+    $("#banner-section .owl-carousel").owlCarousel({
+      loop: true,
+      nav: false,
+      items: 1,
+      center: true,
+      touch: true
+    });
+});
+
+// Post Slider
+$(document).ready(function () {
+  $("#slider-posts .owl-carousel").owlCarousel({
+    nav: false,
+    items: 4,
+    touch: true,
+    margin:15,
+    responsive: {
+      0:{
+        items: 1
+      },
+      600:{
+        items: 2
+      },
+      992:{
+        items: 3
+      },
+      1200: {
+        items: 4
+      }
+    },
+  });
+});
